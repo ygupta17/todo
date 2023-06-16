ac6b82197eedddb7bc597e1d674e0546bfffdf51
Yash Gupta <yash@Yashs-MacBook-Air-3.local>
2023-06-16 13:59:39 -0400

    changes to todo app

--- src/App.js
+++ src/App.js
@@ -31,13 +31,20 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Checkbox from '@mui/material/Checkbox';
 
-function TodoForm({ addTodo }) {
+function TodoForm({ addTodo, selectedTags, handleTagFilterChange, handleResetFilters, setSortOption }) {
   const [title, setTitle] = useState('');
   const [tags, setTags] = useState([]);
   const [tagInput, setTagInput] = useState('');
   const [dueDate, setDueDate] = useState('');
+  // const [sortOption, setSortOption] = useState('');
+  // const [selectedTags, setSelectedTags] = useState([]);
+
+  const filteredTags = tags.filter((tag) => !selectedTags.includes(tag));
+
   
+
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
   };
@@ -82,11 +89,27 @@ function TodoForm({ addTodo }) {
     }
   };
 
+  // const handleSortOptionChange = (e) => {
+  //   setSortOption(e.target.value);
+  // };
+
+  // const handleTagFilterChange = (e) => {
+  //   const selectedTags = Array.from(e.target.selectedOptions, (option) => option.value);
+  //   setSelectedTags(selectedTags);
+  // };
+
+  // const handleResetFilters = () => {
+  //   setSelectedTags([]);
+  // };
+
+
+
   return (
     <div style={{
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
+      flexDirection: 'column'
   }}>
     <Card style={{ width: 500, height: 300 }}>
     <div>
@@ -125,25 +148,75 @@ function TodoForm({ addTodo }) {
         </div>
         
         <div style={{marginLeft: 50, marginTop: 30}}>
-        <button style={{width: 380}} type="submit">Create</button>
+        <button style={{width: 380}} type="submit" onClick={handleSubmit}>Create</button>
         </div>
       </form>
     </div>
+    </Card>
+    
+    <Card style={{ width: 500, height: 100, marginTop: 5, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <button onClick={() => setSortOption('todo')}>todo</button>
+          <button onClick={() => setSortOption('date')}>date</button>
+        </div>
+      </div>
+
     </Card>
     </div>
   );
 }
 
-function TodoList({ todos, toggleTodo }) {
+function TodoList({ todos, toggleTodo, selectedTags, handleTagFilterChange}) {
+  
   return (
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      marginTop: 5
+  }}>
     <div>
       {todos.map((todo, index) => (
-        <div key={index} style={{ backgroundColor: todo.completed ? '#eee' : '#fff' }} onClick={() => toggleTodo(index)}>
-          <h3>{todo.title}</h3>
-          <p>Due Date: {todo.dueDate}</p>
-          <p>Tags: {todo.tags.join(', ')}</p>
-        </div>
+        <Card key={index} style={{ width: 500, height: 150 }}>
+          <CardContent>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Checkbox
+                checked={todo.completed}
+                onChange={() => toggleTodo(index)}
+              />
+              <Typography
+                variant="h6"
+                style={{
+                  textDecoration: todo.completed ? 'line-through' : 'none', fontWeight: 'bold'
+                }}
+              >
+                {todo.title}
+              </Typography>
+            </div>
+            <Typography variant="body2" style={{marginLeft: 370}}>
+              by {todo.dueDate}
+            </Typography>
+            <div>
+              {todo.tags.map((tag, tagIndex) => (
+                <button style={{ 
+                  background: '#ffffff', 
+                  borderWidth: '3px solid black', 
+                  borderRadius: '5px',
+                  display: 'flex', 
+                  alignItems: 'center',
+                  marginLeft: 40}} 
+                  type="button"
+                  > {tag}</button>
+              ))}
+            </div>
+            
+           
+          </CardContent>
+        </Card>
       ))}
+    </div>
     </div>
   );
 }
@@ -163,6 +236,15 @@ function App() {
     setTodos(updatedTodos);
   };
 
+  const handleTagFilterChange = (e) => {
+    const selectedTags = Array.from(e.target.selectedOptions, (option) => option.value);
+    setSelectedTags(selectedTags);
+  };
+
+  const handleResetFilters = () => {
+    setSelectedTags([]);
+  };
+
 
   const filteredTodos = todos.filter((todo) => {
     if (selectedTags.length === 0) {
@@ -172,20 +254,35 @@ function App() {
     }
   });
 
-  const sortedTodos = [...filteredTodos].sort((a, b) => {
-    if (sortOption === 'date') {
-      return new Date(a.dueDate) - new Date(b.dueDate);
-    } else if (sortOption === 'completed') {
-      return a.completed === b.completed ? 0 : a.completed ? 1 : -1;
-    } else {
-      return 0;
-    }
-  });
+  // const sortedTodos = [...filteredTodos].sort((a, b) => {
+  //   if (sortOption === 'date') {
+  //     return new Date(a.dueDate) - new Date(b.dueDate);
+  //   } else if (sortOption === 'todo') {
+  //     return a.todo === b.todo ? 0 : a.todo ? 1 : -1;
+  //   } else {
+  //     return 0;
+  //   }
+  // });
+  if (sortOption === 'todo') {
+    filteredTodos.sort((a, b) => a.title.localeCompare(b.title));
+  } else if (sortOption === 'date') {
+    filteredTodos.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
+  }
 
   return (
     <div>
-      <TodoForm addTodo={addTodo} />
-      <TodoList todos={sortedTodos} toggleTodo={toggleTodo} />
+      <TodoForm 
+        addTodo={addTodo}
+        selectedTags={selectedTags}
+        handleTagFilterChange={handleTagFilterChange}
+        handleResetFilters={handleResetFilters}
+        setSortOption={setSortOption}
+       />
+      <TodoList 
+        todos={filteredTodos} 
+        toggleTodo={toggleTodo}
+        selectedTags={selectedTags}
+        handleTagFilterChange={handleTagFilterChange} />
     </div>
   );
 }
