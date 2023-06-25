1127d68e0d62efec86f718bd21a47c785332e9d7
Yash Gupta <yash@Yashs-Air-3.fios-router.home>
2023-06-25 13:35:10 -0400

    changes to styling and functionality

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import React, { useState } from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Checkbox from '@mui/material/Checkbox';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
 
 function TodoForm({ addTodo, selectedTags, handleTagFilterChange, handleResetFilters, setSortOption }) {
   const [title, setTitle] = useState('');
   const [tags, setTags] = useState([]);
   const [tagInput, setTagInput] = useState('');
   const [dueDate, setDueDate] = useState('');
-  // const [sortOption, setSortOption] = useState('');
-  // const [selectedTags, setSelectedTags] = useState([]);
-
+  const [filterTags, setFilterTags] = useState([])
+ 
   const filteredTags = tags.filter((tag) => !selectedTags.includes(tag));
-
-  
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
@@ -57,6 +29,7 @@ function TodoForm({ addTodo, selectedTags, handleTagFilterChange, handleResetFil
     if (tagInput.trim() !== '') {
       setTags([...tags, tagInput.trim()]);
       setTagInput('');
+      setFilterTags([...filterTags, tagInput])
     }
   };
 
@@ -69,39 +42,32 @@ function TodoForm({ addTodo, selectedTags, handleTagFilterChange, handleResetFil
     setDueDate(e.target.value);
   };
 
+  //if title and date entered create card otherwise alert user to enter title and/or date
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (title.trim() !== '' && dueDate.trim() !== '') {
-      const newTodo = {
-        title: title.trim(),
-        tags: tags,
-        dueDate: dueDate.trim(),
-        completed: false,
-      };
-
-      addTodo(newTodo);
-
-      setTitle('');
-      setTags([]);
-      setTagInput('');
-      setDueDate('');
+    if (title.trim() !== '') {
+      if (dueDate.trim() !== '') {
+        const newTodo = {
+          title: title.trim(),
+          tags: tags,
+          dueDate: dueDate.trim(),
+          completed: false,
+        };
+  
+        addTodo(newTodo);
+  
+        setTitle('');
+        setTags([]);
+        setTagInput('');
+        setDueDate('');
+      } else {
+          alert('Please enter a due date.');
+      }
+    } else {
+        alert('Please enter a title.');
     }
   };
-
-  // const handleSortOptionChange = (e) => {
-  //   setSortOption(e.target.value);
-  // };
-
-  // const handleTagFilterChange = (e) => {
-  //   const selectedTags = Array.from(e.target.selectedOptions, (option) => option.value);
-  //   setSelectedTags(selectedTags);
-  // };
-
-  // const handleResetFilters = () => {
-  //   setSelectedTags([]);
-  // };
-
 
 
   return (
@@ -115,7 +81,7 @@ function TodoForm({ addTodo, selectedTags, handleTagFilterChange, handleResetFil
     <div>
       <form onSubmit={handleSubmit}>
         <div style={{marginTop: 30}}>
-        <label style={{marginLeft: 50}}>
+        <label style={{marginLeft: 50, color: '#2944cc'}}>
           Title
           <input style ={{marginLeft: 200, marginRight: 50}} type="text" value={title} onChange={handleTitleChange} required />
         </label>
@@ -123,24 +89,24 @@ function TodoForm({ addTodo, selectedTags, handleTagFilterChange, handleResetFil
         </div>
 
         <div style={{marginTop: 30}}>
-        <label style={{marginLeft: 50}}>
+        <label style={{marginLeft: 50, color: '#2944cc'}}>
           Tags 
           <input style={{marginLeft: 90}} type="text" value={tagInput} placeholder="Ex) Grocery, School, ..." onChange={handleTagInputChange} />
-          <button type="button" onClick={handleCreateTag}>Create new tag</button>
+          <button style={{backgroundColor: '#a5d4e8', color: '#2944cc'}} type="button" onClick={handleCreateTag}>Create new tag</button>
         </label>
         </div>
 
         <div style={{marginLeft: 50, marginTop: 30}}>
           {tags.map((tag, index) => (
-            
-              
-              <button style={{marginRight: 5, background: '#ffffff', borderWidth: '3px solid black', borderRadius: '5px'}} type="button" onClick={() => handleDeleteTag(tag)}>x {tag}</button>
-            
+              <button style={{marginRight: 5, background: '#ffffff', borderWidth: '3px solid black', borderRadius: '5px', color: '#2944cc'}} 
+              type="button" 
+              onClick={() => handleDeleteTag(tag)}>
+                x {tag}</button>  
           ))}
         </div>
 
         <div style={{marginTop: 30}}>
-        <label style={{marginLeft: 50}}>
+        <label style={{marginLeft: 50, color: '#2944cc'}}>
           Due Date
           <input style={{marginLeft: 190, marginRight: 50}} type="date" value={dueDate} onChange={handleDueDateChange} required />
         </label>
@@ -148,7 +114,7 @@ function TodoForm({ addTodo, selectedTags, handleTagFilterChange, handleResetFil
         </div>
         
         <div style={{marginLeft: 50, marginTop: 30}}>
-        <button style={{width: 380}} type="submit" onClick={handleSubmit}>Create</button>
+        <button style={{width: 380, backgroundColor: '#a5d4e8', color: '#2944cc'}} type="submit" onClick={handleSubmit}>Create</button>
         </div>
       </form>
     </div>
@@ -157,17 +123,40 @@ function TodoForm({ addTodo, selectedTags, handleTagFilterChange, handleResetFil
     <Card style={{ width: 500, height: 100, marginTop: 5, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <button onClick={() => setSortOption('todo')}>todo</button>
-          <button onClick={() => setSortOption('date')}>date</button>
+          <button style={{backgroundColor: '#a5d4e8', color: '#2944cc'}} onClick={() => setSortOption('todo')}>todo</button>
+          <button style={{backgroundColor: '#a5d4e8', color: '#2944cc'}} onClick={() => setSortOption('date')}>date</button>
         </div>
       </div>
-
     </Card>
+
+      <Card style={{ width: 500, height: 100, }}>
+        <div style={{display: 'flex',
+justifyContent: 'center',
+alignItems: 'center'}}>
+        <FormControl>
+          <Select
+            style={{color: '#2944cc'}}
+            multiple
+            value={selectedTags}
+            onChange={handleTagFilterChange}
+          >
+            {filterTags.map((tag) => (
+              <MenuItem key={tag} value={tag} style={{color: '#2944cc'}}>
+                {tag}
+              </MenuItem>
+            ))}
+          </Select>
+          <div>
+            <button style={{backgroundColor: '#a5d4e8', color: '#2944cc'}} onClick={handleResetFilters}>Reset Filters</button>
+          </div>
+        </FormControl>
+        </div>
+      </Card>
     </div>
   );
 }
 
-function TodoList({ todos, toggleTodo, selectedTags, handleTagFilterChange}) {
+function TodoList({ todos, toggleTodo, selectedTags}) {
   
   return (
     <div style={{
@@ -183,19 +172,19 @@ function TodoList({ todos, toggleTodo, selectedTags, handleTagFilterChange}) {
           <CardContent>
             <div style={{ display: 'flex', alignItems: 'center' }}>
               <Checkbox
+                style={{color: '#2944cc'}}
                 checked={todo.completed}
                 onChange={() => toggleTodo(index)}
               />
               <Typography
-                variant="h6"
                 style={{
-                  textDecoration: todo.completed ? 'line-through' : 'none', fontWeight: 'bold'
+                  textDecoration: todo.completed ? 'line-through' : 'none', fontWeight: 'bold', color: '#2944cc'
                 }}
               >
                 {todo.title}
               </Typography>
             </div>
-            <Typography variant="body2" style={{marginLeft: 370}}>
+            <Typography style={{marginLeft: 370, color: '#2944cc'}}>
               by {todo.dueDate}
             </Typography>
             <div>
@@ -206,13 +195,12 @@ function TodoList({ todos, toggleTodo, selectedTags, handleTagFilterChange}) {
                   borderRadius: '5px',
                   display: 'flex', 
                   alignItems: 'center',
-                  marginLeft: 40}} 
+                  marginLeft: 40,
+                  color: '#2944cc'}} 
                   type="button"
                   > {tag}</button>
               ))}
             </div>
-            
-           
           </CardContent>
         </Card>
       ))}
@@ -237,14 +225,12 @@ function App() {
   };
 
   const handleTagFilterChange = (e) => {
-    const selectedTags = Array.from(e.target.selectedOptions, (option) => option.value);
-    setSelectedTags(selectedTags);
+    setSelectedTags(e.target.value);
   };
 
   const handleResetFilters = () => {
     setSelectedTags([]);
   };
-
 
   const filteredTodos = todos.filter((todo) => {
     if (selectedTags.length === 0) {
@@ -254,15 +240,6 @@ function App() {
     }
   });
 
-  // const sortedTodos = [...filteredTodos].sort((a, b) => {
-  //   if (sortOption === 'date') {
-  //     return new Date(a.dueDate) - new Date(b.dueDate);
-  //   } else if (sortOption === 'todo') {
-  //     return a.todo === b.todo ? 0 : a.todo ? 1 : -1;
-  //   } else {
-  //     return 0;
-  //   }
-  // });
   if (sortOption === 'todo') {
     filteredTodos.sort((a, b) => a.title.localeCompare(b.title));
   } else if (sortOption === 'date') {
@@ -282,7 +259,8 @@ function App() {
         todos={filteredTodos} 
         toggleTodo={toggleTodo}
         selectedTags={selectedTags}
-        handleTagFilterChange={handleTagFilterChange} />
+        handleTagFilterChange={handleTagFilterChange}
+         />
     </div>
   );
 }
